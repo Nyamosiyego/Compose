5a873d1a251a0a84e5745613d019bff65d56c981
Edmond <edmond.nyamosi@strathmore.edu>
2023-10-05 18:52:32 +0300

    new composable

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.compose
 
+import SampleData
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -27,21 +29,22 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(
-                msg = "Hello"
-            )
-            }
+            Conversation(SampleData.conversationSample)
+        }
         }
     }
+data class Message(val author: String, val body: String)
+
 
 @Composable
-fun MessageCard(msg: String) {
+fun MessageCard(msg: Message) {
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.kermit),
@@ -59,13 +62,14 @@ fun MessageCard(msg: String) {
         // surfaceColor will be updated gradually from one color to the other
         val surfaceColor by animateColorAsState(
             if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            label = ""
         )
         
         // We toggle the isExpanded variable when we click on this Column
     
         Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
-                text = "s",
+                text = msg.author,
                 color = MaterialTheme.colorScheme.secondary,
                 style = MaterialTheme.typography.titleSmall
             )
@@ -78,10 +82,12 @@ fun MessageCard(msg: String) {
                 // surfaceColor color will be changing gradually from primary to surface
                 color = surfaceColor,
                 // animateContentSize will change the Surface size gradually
-                modifier = Modifier.animateContentSize().padding(1.dp)
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
             ) {
                 Text(
-                    text = "Was good",
+                    text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
                     // If the message is expanded, we display all its content
                     // otherwise we only display the first line
@@ -94,3 +100,21 @@ fun MessageCard(msg: String) {
 }
 
 
+@Preview
+@Composable
+fun PreviewConversation() {
+    Conversation(SampleData.conversationSample)
+}
+
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    Surface {
+        LazyColumn {
+            items(messages.size) { index ->
+                MessageCard(messages[index])
+            }
+        }
+    }
+    
+}
